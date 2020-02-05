29b5783bd8493c7af053e3e43907fd7ba7e384ea
sucodlaranja <55655155+sucodlaranja@users.noreply.github.com>
2020-02-05 16:37:46 +0000

    Add files via upload

---
+++ Tarefa2/cmake-build-debug/CMakeFiles/Tarefa2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarefa2.dir/main.c.o"
+  "Tarefa2"
+  "Tarefa2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Tarefa2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
